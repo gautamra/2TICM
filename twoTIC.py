2dc29e6333e751ecd95a36697d57c3a29dff424f
GR <gautam.rai@uni-hamburg.de>
2024-12-05 11:39:15 +0100

    Add experimental three color implementation

--- twoTIC.py
+++ twoTIC.py
@@ -7,3 +7,11 @@ def get_rgba(intensity, character, data_max, lim=1.0, power=1.0):
     blue = red[:,:,[2,1,0,3]]
     rgba = (1-character)[:,:,None]*red+(character[:,:,None])*blue
     return red, blue, rgba
+
+def get_rgba_three(intensity, character, data_max, lim=1.0, power=1.0):
+    cmap = mpl.cm.get_cmap('gist_heat_r')
+    red = cmap(np.power(intensity/data_max*lim, power))
+    blue = red[:,:,[2,1,0,3]]
+    green = red[:,:,[1,0,2,3]]
+    rgba = (character)[:,:,0,None]*red+(character[:,:,1,None])*green+(character[:,:,2,None])*blue
+    return red, green, blue, rgba
